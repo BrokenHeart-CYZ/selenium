a1af55f8a041e7817ee873337921f64a1142efa3
BrokenHeart-CYZ <3096979337@qq.com>
2021-05-28 16:07:42 +0800

    fourth commit

--- test_selenium.py
+++ test_selenium.py
@@ -9,19 +9,18 @@ driver = webdriver.Chrome(executable_path='C:\\Users\\dell\\AppData\\Local\\Goog
 options=options,service_log_path=os.devnull)
 # driver.get('http://www.baidu.com')
 # sleep(3)
-#def test_demo1():
+def test_demo1():
     # options = webdriver.ChromeOptions()
     # options.add_argument('–log-level=3')
     # options.add_experimental_option('excludeSwitches', ['enable-logging'])
     # driver = webdriver.Chrome(executable_path='C:\\Users\\dell\\AppData\\Local\\Google\\Chrome\\Application\\chromedriver.exe',
     # options=options,service_log_path=os.devnull)
-driver.get('http://www.baidu.com')
-sleep(3)
-driver.find_element_by_id("kw").send_keys("selenium")
-driver.find_element_by_id("su").click()
-sleep(3)
-#def teardown_module():
-driver.quit()
+    driver.get('http://www.baidu.com')
+    sleep(3)
+    driver.find_element_by_id("kw").send_keys("selenium")
+    driver.find_element_by_id("su").click()
+    sleep(3)
+    driver.quit()
 
 
 
